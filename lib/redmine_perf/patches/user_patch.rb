69108619f6b08b3036dc9440ae0c2336835c4978
bilel <bilel.kedidi@gmail.com>
2016-12-29 20:32:25 +0100

    add new performance

--- lib/redmine_perf/patches/user_patch.rb
+++ lib/redmine_perf/patches/user_patch.rb
@@ -10,6 +10,46 @@ module  RedminePerf
         base.class_eval do
           alias_method_chain :projects_by_role, :perf
           alias_method_chain :reload, :memberships
+          def allowed_to?(action, context, options={}, &block)
+            if context && context.is_a?(Project)
+              return false unless context.allows_to?(action)
+              # Admin users are authorized for anything else
+              return true if admin?
+
+              roles = roles_for_project(context)
+              return false unless roles
+              roles.any? {|role|
+                (context.is_public? || role.member?) &&
+                    role.allowed_to?(action) &&
+                    (block_given? ? yield(role, self) : true)
+              }
+            elsif context && context.is_a?(Array)
+              if context.empty?
+                false
+              else
+                # Authorize if user is authorized on every element of the array
+                context.map {|project| allowed_to?(action, project, options, &block)}.reduce(:&)
+              end
+            elsif context
+              raise ArgumentError.new("#allowed_to? context argument must be a Project, an Array of projects or nil")
+            elsif options[:global]
+              # Admin users are always authorized
+              return true if admin?
+
+              # authorize if user has at least one role that has this permission
+              # rls = memberships.collect {|m| m.roles}.flatten.uniq
+              # rls << (self.logged? ? Role.non_member : Role.anonymous)
+              # rls.any? {|role|
+              rls = user_roles.to_a
+              rls << builtin_role
+              rls.any? {|role|
+                role.allowed_to?(action) &&
+                    (block_given? ? yield(role, self) : true)
+              }
+            else
+              false
+            end
+          end
 
         end
       end
@@ -18,7 +58,12 @@ module  RedminePerf
     end
 
     module InstanceMethods
+      def user_roles
+        @roles ||= Role.joins(members: :project).where(["#{Project.table_name}.status <> ?", Project::STATUS_ARCHIVED]).where(Member.arel_table[:user_id].eq(id)).uniq
+      end
+
       def reload_with_memberships(*args)
+        @roles = nil
         @memberships = nil
         @user_statement_role = {}
         reload_without_memberships(*args)
@@ -32,7 +77,8 @@ module  RedminePerf
 
       def user_statement_role(permission)
         @user_statement_role ||= {}
-        @user_statement_role[permission] ||= begin
+        return @user_statement_role[permission] if @user_statement_role[permission]
+        @user_statement_role[permission] = begin
           @user_statement_role[permission] = {}
           self.projects_by_role.each do |role, project_ids|
             if role.allowed_to?(permission) && project_ids.any?
